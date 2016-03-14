c3e51a956a09fab8ceffe58197f1e3b2d1bafdf6
Roeland Kuipers <rkuipers@schubergphilis.com>
2016-03-14 12:30:18 +0100

    first commit

---
+++ chef/chef_loader.py
@@ -0,0 +1,41 @@
+
+import chef
+import json
+import pprint
+import urllib2
+
+bla = chef.autoconfigure()
+
+# client = bla.client
+
+nodes = chef.Node.list()
+
+d = dict()
+summ = []
+
+for node in nodes:
+    print node
+
+    ohai = chef.Node(node).automatic.to_dict()
+    summ.append(ohai)
+
+    d['nodes'] = summ
+
+
+json_output = json.dumps(d)
+print json_output
+
+req = urllib2.Request('http://localhost:8081/v1/data/ohai')
+req.add_header('Content-Type', 'application/json')
+
+response = urllib2.urlopen(req, json_output)
+
+print response
+
+
+        # print attribute.get_dotted()
+        # pprint.pprint(attribute)
+        # # json_output = json.dumps(attribute)
+        #
+        # # print json_output
+        #
